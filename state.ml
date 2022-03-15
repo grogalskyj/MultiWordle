ec75b70a98b6e8d8798a2e8468cca9a3840a80f1
Charles <cw877@cornell.edu>
2022-03-14 21:16:17 -0400

    updated with complete state interface and documentation

--- state.ml
+++ state.ml
@@ -11,3 +11,12 @@ type game_status =
 let init_game_state (num_letters : int) : state =
   {word = num_letters |> generate_word_bank input_json |> choose_random_word;
   remaining_guesses = 6; curr_guess = ""}
+let update_state (game_state : state) (new_guess : string) : state =
+  {word = game_state.word; 
+  remaining_guesses = game_state.remaining_guesses - 1; 
+  curr_guess = new_guess}
+let check_game_over (game_state : state) : bool =
+  if game_state.curr_guess = game_state.word then
+    let _ = game_over in true
+  else if game_state.remaining_guesses = 0 then true
+  else false
