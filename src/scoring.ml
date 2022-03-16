5dff1643847a0a2d2a94a597df90d013b0bfc44a
Jason Katz <jak496@cornell.edu>
2022-03-16 17:58:39 -0400

    Implemented score_input and give_feedback in scoring.ml

--- src/scoring.ml
+++ src/scoring.ml
@@ -1,7 +1,5 @@
 let rec score_input user_input correct_word start_index = 
   match user_input with
-  | _ -> "hi"
-  (**
   | "" -> ""
   | _ -> if
     String.lowercase_ascii (String.sub user_input start_index 1) = 
@@ -16,21 +14,16 @@ let rec score_input user_input correct_word start_index =
     "_ " 
   ^  
     score_input (String.sub (user_input) (start_index+1) ((String.length (user_input))-1))
-    (String.sub (correct_word) (start_index+1) ((String.length (correct_word))-1)) (start_index+1) *)
+    (String.sub (correct_word) (start_index+1) ((String.length (correct_word))-1)) (start_index+1)
 
 
 let give_feedback game_state user_input correct_word = 
   if game_state.remaining_guesses = 0 then feedback_helper "OUT OF LIVES" else
-    "hi"
+    if game_state.remaining_guesses >= 0 && String.lowercase_ascii user_input <> String.lowercase_ascii correct_word then
+      feedback_helper "KEEP GOING" else feedback_helper "YOU WIN"
 
 
-let feedback_helper user_input correct_word lives feedback= function
-  | "OUT OF LIVES" -> "You guessed "^user_input^". The correct word was "^correct_word^". You are out of lives. GAME OVER."
-  | "YOU WIN" -> "You guessed the word "^correct_word^" with "lives.to_String^" lives remaining. YOU WIN!"
-  | "KEEP GOING" ->"You guessed the word "^user_input^". Here is your feedback: "^feedback^". You have "^lives.to_String^" lives remaining."
-    
-  
-  (** Messages to come
+(**Intended Messages
     
     OUT OF LIVES: You guessed ___. The correct word was ___. You are out of lives. GAME OVER.
     
@@ -39,4 +32,12 @@ let feedback_helper user_input correct_word lives feedback= function
     KEEP GOING: You guessed ___. Here is your feedback: ___. You have ___ guesses remaining.
     
     *)
+let feedback_helper user_input correct_word lives feedback= function
+  | "OUT OF LIVES" -> "You guessed "^user_input^". The correct word was "^correct_word^". You are out of lives. GAME OVER."
+  | "YOU WIN" -> "You guessed the word "^correct_word^" with "lives.to_String^" lives remaining. YOU WIN!"
+  | "KEEP GOING" ->"You guessed the word "^user_input^". Here is your feedback: "^feedback^". You have "^lives.to_String^" lives remaining."
+    
+  
+ 
+
 
