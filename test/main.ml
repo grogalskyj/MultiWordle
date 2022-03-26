d4b4750414e23cbd281d51cc5d96d7be58a15e60
Charles <cw877@cornell.edu>
2022-03-26 13:16:45 -0400

    finished test cases for state

--- test/main.ml
+++ test/main.ml
@@ -21,8 +21,11 @@ let score_input_tests =
 
   let word_length = 5
   let st1 = init_game_state word_length
-  let new_guess = "epoxy"
-  let st2 = update_game_state st1 new_guess
+  let new_guess1 = "epoxy"
+  let st2 = update_game_state st1 new_guess1
+  let new_guess2 = st2.word
+  let st3 = update_game_state st2 new_guess2
+  let st4 = {st3 with remaining_guesses = 0}
   let state_tests = [
     ("The word in newly-initiated state is of the proper length" >:: fun _ -> assert_equal word_length (String.length st1.word));
     ("The remaining guesses of newly-initiated state is 6" >:: fun _ -> assert_equal 6 st1.remaining_guesses);
@@ -30,7 +33,11 @@ let score_input_tests =
 
     ("Updating a game state does not change the designated word" >:: fun _ -> assert_equal st1.word st2.word);
     ("Updating a game state reduces the number of guesses by 1" >:: fun _ -> assert_equal st2.remaining_guesses (st1.remaining_guesses - 1));
-    ("Updating a game state changes the current guess to the new guess" >:: fun _ -> assert_equal new_guess st2.curr_guess)
+    ("Updating a game state changes the current guess to the new guess" >:: fun _ -> assert_equal new_guess1 st2.curr_guess);
+
+    ("An ongoing game where there are remaining guesses left and where the word has not beet guessed is not over" >:: fun _ -> assert_equal false (check_game_over st2));
+    ("A game is over if the designated word has been guessed" >:: fun _ -> assert_equal true (check_game_over st3));
+    ("A game is over if there are no remaining guesses" >:: fun _ -> assert_equal true (check_game_over st4))
   ]
   let suite =
     "test suite for MultiWordle"
