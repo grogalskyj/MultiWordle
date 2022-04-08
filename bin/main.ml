4601a53ee5ff15f68898ad9dd37343e7a3acd2ff
George Rogalskyj <gar232@cornell.edu>
2022-04-08 12:47:39 -0400

    Added multiplayer option with appropriate interface... will now work on gameplay between two players

--- bin/main.ml
+++ bin/main.ml
@@ -46,24 +46,26 @@ let alphabet =
    check_game_over new_game_state then print_endline "Please play\n
    again!" else game_iter new_game_state) *)
 
-let rec game_iter game_state =
+let rec game_iter_one game_state =
   print_endline "Your guess: ";
   print_string "> ";
   let guess = read_line () in
   if String.length guess <> String.length game_state.word then (
     print_endline
       "You did not enter a string of valid length. Please try again.";
-    game_iter game_state)
+    game_iter_one game_state)
   else if is_word guess game_state.dictionary = false then (
     print_endline "You did not enter a valid word. Please try again.";
-    game_iter game_state)
+    game_iter_one game_state)
   else (
     print_colored_feedback (score_input guess game_state.word);
     let new_game_state = update_game_state game_state guess in
     print_word_bank new_game_state.char_bank alphabet guess;
     if check_game_over new_game_state then
       print_endline "Please play again!"
-    else game_iter new_game_state)
+    else game_iter_one new_game_state)
+
+let game_iter_two game_state = game_iter_one game_state
 
 (* let rec check_acceptable (accept : int list) (input : string) = let
    input_int = int_of_string input in match accept with | [] -> false |
@@ -75,9 +77,19 @@ let rec game_iter game_state =
    10: "; let s = read_line () in if check_acceptable acceptable_input s
    then int_of_string s else 5 *)
 
-let play_game (num : int) () =
+let rec play_game (num : int) () =
   let game_state = init_game_state num in
-  game_iter game_state
+  ANSITerminal.print_string [ ANSITerminal.red ]
+    "1 player or 2 player game? Enter '1' for one player, and '2' for \
+     two player.\n";
+  print_string "> ";
+  let input = read_line () in
+  match input with
+  | "1" -> game_iter_one game_state
+  | "2" -> game_iter_two game_state
+  | _ ->
+      print_endline "You did not enter a valid command";
+      play_game num ()
 
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
