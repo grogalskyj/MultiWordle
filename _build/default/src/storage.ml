4aa0d7182ad4628dca68fa048e7e4dd099cbdb17
Jason Katz <jak496@cornell.edu>
2022-05-16 15:51:01 -0400

    Implemented Wordle statistics feature in storage.ml

--- _build/default/src/storage.ml
+++ _build/default/src/storage.ml
@@ -1,2 +1,27 @@
-(* let player_database = [] let games_played a b = 5 let average_guesses
-   a b = 5 *)
+open Player
+
+type player_database = (string * player) list
+
+let rec get_usernames (database : player_database) : string list =
+  match database with
+  | [] -> []
+  | [ (h, _) ] -> [ h ]
+  | (h, _) :: t -> h :: get_usernames t
+
+let get_player_record (username : string) (database : player_database) :
+    player =
+  let usernames_list = get_usernames database in
+  if List.mem username usernames_list then List.assoc username database
+  else failwith "Player does not exist"
+
+let games_played (username : string) (database : player_database) : int
+    =
+  let player_record = get_player_record username database in
+  List.length player_record.game_history
+
+let average_guesses (username : string) (database : player_database) :
+    int =
+  let player_record = get_player_record username database in
+  5
+
+let guess_trend username player_database = 5
--- src/storage.mli
+++ src/storage.mli
@@ -1,9 +1,19 @@
-(* open Player
+open Player
 
-   type player_database = string list
+type player_database = (string * player) list
+(** [player_database] is a list containing each player's username and
+    the corresponding player type that matches the corresponding
+    username*)
 
-   val games_played : string -> (int * int) list -> int (**
-   [games_played username score_history] calculates the total number of
-   games that a specific user has played.*)
+val games_played : string -> player_database -> int
+(** [games_played username player_database] calculates the total number
+    of games that a specific user has played.*)
 
-   val average_guesses : string -> int -> int *)
+val average_guesses : string -> player_database -> int
+(** [average_guesses username player_database] calculates the average
+    number of guesses it takes a specific user to guess a word.*)
+
+val guess_trend : string -> player_database -> int
+(** [recent_guess-trend username player_database] calculates the average
+    number of guess it has taken a specific user to guess a word in
+    their last 3 games played. *)
